a99205ce0452b4ff1f650e2cd31709e1b64a4c2e
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-04-20 08:29:18 -0700

    Update i18n.js

--- frontend/lib/i18n.js
+++ frontend/lib/i18n.js
@@ -2,24 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+// Import only working languages for now
 import en from '../locales/en.json';
 import am from '../locales/am.json';
-import ti from '../locales/ti.json';
-import so from '../locales/so.json';
-import aa from '../locales/aa.json';
-import wal from '../locales/wal.json';
-import hdy from '../locales/hdy.json';
-import sid from '../locales/sid.json';
 
 const resources = {
   en: { translation: en },
-  am: { translation: am },
-  ti: { translation: ti },
-  so: { translation: so },
-  aa: { translation: aa },
-  wal: { translation: wal },
-  hdy: { translation: hdy },
-  sid: { translation: sid }
+  am: { translation: am }
 };
 
 i18n
