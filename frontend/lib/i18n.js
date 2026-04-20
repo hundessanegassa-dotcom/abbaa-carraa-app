050297b1295443b5654a3cdc3b724519227b4a37
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-04-20 02:41:09 -0700

    Clean up language imports in i18n.js
    Removed unused language imports for 'om' and 'ti'.

--- frontend/lib/i18n.js
+++ frontend/lib/i18n.js
@@ -2,18 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Import all translations
+// Import translations
 import en from '../locales/en.json';
 import am from '../locales/am.json';
-import om from '../locales/om.json';
-import ti from '../locales/ti.json';
 import so from '../locales/so.json';
 
 const resources = {
   en: { translation: en },
   am: { translation: am },
-  om: { translation: om },
-  ti: { translation: ti },
   so: { translation: so }
 };
 
