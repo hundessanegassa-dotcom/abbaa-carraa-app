fd49c39f743b491427c4bdb017b3bcdf078d62e3
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-04-20 07:01:45 -0700

    Update i18n.js

--- frontend/lib/i18n.js
+++ frontend/lib/i18n.js
@@ -2,15 +2,27 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Import translations
+// Import all 9 Ethiopian languages
 import en from '../locales/en.json';
 import am from '../locales/am.json';
+import om from '../locales/om.json';
+import ti from '../locales/ti.json';
 import so from '../locales/so.json';
+import aa from '../locales/aa.json';
+import wal from '../locales/wal.json';
+import hdy from '../locales/hdy.json';
+import sid from '../locales/sid.json';
 
 const resources = {
   en: { translation: en },
   am: { translation: am },
-  so: { translation: so }
+  om: { translation: om },
+  ti: { translation: ti },
+  so: { translation: so },
+  aa: { translation: aa },
+  wal: { translation: wal },
+  hdy: { translation: hdy },
+  sid: { translation: sid }
 };
 
 i18n
@@ -23,6 +35,10 @@ i18n
     interpolation: {
       escapeValue: false,
     },
+    detection: {
+      order: ['localStorage', 'cookie', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+    }
   });
 
 export default i18n;
