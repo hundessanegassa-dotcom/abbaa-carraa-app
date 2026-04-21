9a70bb66ff97fa76a43ae8338811b141a813ab09
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-04-21 02:10:31 -0700

    Clean up i18n language imports and enable debug
    Removed unused Ethiopian language imports and updated resources object. Enabled debug mode for i18n.

--- frontend/lib/i18n.js
+++ frontend/lib/i18n.js
@@ -2,27 +2,12 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Import all 9 Ethiopian languages
 import en from '../locales/en.json';
 import am from '../locales/am.json';
-import om from '../locales/om.json';
-import ti from '../locales/ti.json';
-import so from '../locales/so.json';
-import aa from '../locales/aa.json';
-import wal from '../locales/wal.json';
-import hdy from '../locales/hdy.json';
-import sid from '../locales/sid.json';
 
 const resources = {
   en: { translation: en },
-  am: { translation: am },
-  om: { translation: om },
-  ti: { translation: ti },
-  so: { translation: so },
-  aa: { translation: aa },
-  wal: { translation: wal },
-  hdy: { translation: hdy },
-  sid: { translation: sid }
+  am: { translation: am }
 };
 
 i18n
@@ -31,7 +16,7 @@ i18n
   .init({
     resources,
     fallbackLng: 'en',
-    debug: false,
+    debug: true,
     interpolation: {
       escapeValue: false,
     },
