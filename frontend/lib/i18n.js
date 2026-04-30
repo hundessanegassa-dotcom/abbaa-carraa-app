976ee1ae555222345bf3bcdc7feb815e8f3e64b0
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-04-30 08:27:25 -0700

    Refactor i18n.js to use require for JSON files

--- frontend/lib/i18n.js
+++ frontend/lib/i18n.js
@@ -2,17 +2,27 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Use require for JSON files (works better in some Next.js versions)
+// Use require instead of import for more reliable JSON parsing
+const en = require('../locales/en.json');
+const am = require('../locales/am.json');
+const om = require('../locales/om.json');
+const ti = require('../locales/ti.json');
+const so = require('../locales/so.json');
+const aa = require('../locales/aa.json');
+const wal = require('../locales/wal.json');
+const hdy = require('../locales/hdy.json');
+const sid = require('../locales/sid.json');
+
 const resources = {
-  en: { translation: require('../locales/en.json') },
-  am: { translation: require('../locales/am.json') },
-  om: { translation: require('../locales/om.json') },
-  ti: { translation: require('../locales/ti.json') },
-  so: { translation: require('../locales/so.json') },
-  aa: { translation: require('../locales/aa.json') },
-  wal: { translation: require('../locales/wal.json') },
-  hdy: { translation: require('../locales/hdy.json') },
-  sid: { translation: require('../locales/sid.json') }
+  en: { translation: en },
+  am: { translation: am },
+  om: { translation: om },
+  ti: { translation: ti },
+  so: { translation: so },
+  aa: { translation: aa },
+  wal: { translation: wal },
+  hdy: { translation: hdy },
+  sid: { translation: sid }
 };
 
 i18n
@@ -24,8 +34,8 @@ i18n
     debug: false,
     interpolation: { escapeValue: false },
     detection: {
-      order: ['localStorage', 'cookie', 'navigator', 'htmlTag'],
-      caches: ['localStorage'],
+      order: ['localStorage', 'cookie', 'navigator'],
+      caches: ['localStorage']
     },
     react: { useSuspense: false }
   });
