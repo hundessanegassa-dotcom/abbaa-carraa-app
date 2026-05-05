426066ab3ff17a1c09ed1c505c18a344ed987a82
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-05-05 07:41:52 -0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,24 +6,25 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
-      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-      handler: 'CacheFirst',
+      urlPattern: /^https:\/\/ruqfgsnhvrckbvibpsyu\.supabase\.co\/rest\/v1\/pools.*/i,
+      handler: 'StaleWhileRevalidate',
       options: {
-        cacheName: 'google-fonts',
+        cacheName: 'pools-cache',
         expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 365
+          maxEntries: 50,
+          maxAgeSeconds: 7 * 24 * 60 * 60 // 7 days
         }
       }
     },
     {
-      urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-      handler: 'CacheFirst',
+      urlPattern: /^https:\/\/ruqfgsnhvrckbvibpsyu\.supabase\.co\/rest\/v1\/.*/i,
+      handler: 'NetworkFirst',
       options: {
-        cacheName: 'google-fonts',
+        cacheName: 'supabase-api',
+        networkTimeoutSeconds: 10,
         expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 365
+          maxEntries: 100,
+          maxAgeSeconds: 24 * 60 * 60
         }
       }
     },
@@ -35,18 +36,6 @@ const withPWA = require('next-pwa')({
         expiration: {
           maxEntries: 60,
           maxAgeSeconds: 30 * 24 * 60 * 60
-        }
-      }
-    },
-    {
-      urlPattern: /^https:\/\/ruqfgsnhvrckbvibpsyu\.supabase\.co\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'supabase-api',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 24 * 60 * 60
         }
       }
     },
@@ -65,7 +54,7 @@ const withPWA = require('next-pwa')({
       urlPattern: /\/.*/i,
       handler: 'NetworkFirst',
       options: {
-        cacheName: 'pages',
+        cacheName: 'pages-cache',
         expiration: {
           maxEntries: 50,
           maxAgeSeconds: 24 * 60 * 60
