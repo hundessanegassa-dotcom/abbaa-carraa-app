0d4ab701a23289ddecfaa9b40376068b00617c74
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-05-05 07:44:47 -0700

    Modify document meta tags for PWA support
    Updated the apple-mobile-web-app-status-bar-style and added viewport meta tag.

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,32 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
-        
-        {/* Theme Color */}
         <meta name="theme-color" content="#10b981" />
-        
-        {/* Apple iOS Support */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Abbaa Carraa" />
-        
-        {/* Apple Touch Icon */}
         <link rel="apple-touch-icon" href="/images/icon-192x192.png" />
-        
-        {/* Favicon for older browsers */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/icon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/icon-16x16.png" />
-        
-        {/* Preconnect for fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        
-        {/* SEO Meta Tags */}
-        <meta name="description" content="A community-driven prize and contribution platform for Ethiopia" />
-        <meta name="keywords" content="prize pool, lottery, community, Ethiopia, Telebirr, CBE Birr" />
-        <meta name="author" content="Abbaa Carraa" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </Head>
       <body>
         <Main />
