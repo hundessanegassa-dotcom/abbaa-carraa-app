ae626b96b1ade207cb9eda1f0ec296d1145937be
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-05-11 01:43:41 -0700

    Add Test component to display build status

---
+++ frontend/pages/test.js
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-green-600">Build is working!</h1>
+    </div>
+  );
+}
