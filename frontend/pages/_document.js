659bef4ee1f94036e9ff95db6ada3eaca0cc4dd7
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-05-12 02:35:07 -0700

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#10b981" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+       <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Abbaa Carraa" />
         <link rel="apple-touch-icon" href="/images/icon-192x192.png" />
