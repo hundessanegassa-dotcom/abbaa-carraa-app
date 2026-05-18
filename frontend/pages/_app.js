5613de662614eeaa94489bdd28586a2b7b2032f7
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-05-18 09:15:44 -0700

    Add Head component for document metadata

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../lib/i18n';
+import Head from 'next/head';  // ← ADD THIS LINE
 
 // Dynamically import components that use browser-only APIs
 const Navbar = dynamic(() => import('../components/Navbar'), { ssr: false });
@@ -33,16 +34,22 @@ function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
       <I18nextProvider i18n={i18n}>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-grow">
-            <Component {...pageProps} />
-          </main>
-          <Footer />
-          <LanguageToggle />
-          <ChatBot />
-          <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
-        </div>
+        <>
+          <Head>  {/* ← ADD THIS ENTIRE Head SECTION */}
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎁</text></svg>" />
+            <link rel="apple-touch-icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎁</text></svg>" />
+          </Head>
+          <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Component {...pageProps} />
+            </main>
+            <Footer />
+            <LanguageToggle />
+            <ChatBot />
+            <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
+          </div>
+        </>
       </I18nextProvider>
     </QueryClientProvider>
   );
