f888cbeadc5f1be470f283fe59d914d8f256c228
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-05-19 06:41:46 -0700

    Add dynamic and fetchCache exports to dashboard.js

--- frontend/pages/admin/dashboard.js
+++ frontend/pages/admin/dashboard.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 import BackButton from '../../components/BackButton';
 import { useState, useEffect } from 'react';
 import { supabase } from '../../lib/supabase';
