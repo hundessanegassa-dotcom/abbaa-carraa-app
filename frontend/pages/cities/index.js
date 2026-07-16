a080ae614c38fa357196f730a78bf0d4d96a4535
hundessanegassa-dotcom <hundessanegassa@gmail.com>
2026-07-16 00:49:25 -0700

    Update import path for city data functions

--- frontend/pages/cities/index.js
+++ frontend/pages/cities/index.js
@@ -2,7 +2,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
-import { getAllCities, getCityData } from './[cityId]';
+import { getAllCities, getCityData } from '../../lib/cityData';
 
 // Get all cities dynamically
 const ethiopianCities = getAllCities();
